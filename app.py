99da816a2dfc51f87c768074ab4f8aa5d2abb3e4
Mukund <pendemmukund@gmail.com>
2026-09-20 15:22:56 +0530

    Added job description form

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/",methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        job_description = request.form["job_description"]
+        print(job_description)
     return render_template("index.html")
 
 
